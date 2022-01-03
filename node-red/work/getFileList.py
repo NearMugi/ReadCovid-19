99a8bd757e6d9134ac3a01bc54a93c713bd17ec9
teppei <niamugi@gmail.com>
2022-01-03 20:57:44 +0900

    getFileList のディレクトリ指定が誤っていた個所を修正

--- node-red/work/getFileList.py
+++ node-red/work/getFileList.py
@@ -1,10 +1,6 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-
-
 # %% [markdown]
 # # フォルダ内のファイルリスト(Json型)を出力する
+
 # %% [markdown]
 # ## import
 
@@ -13,16 +9,19 @@ import sys
 import os
 import json
 from WebScrapingTool import Base_UserFunction as uf
+import comFunction
 
 # %% [markdown]
 # ## フォルダ内のファイル名を取得する
 
 # %%
+
+
 class getFileList:
     # コンストラクタ
     def __init__(self):
         pass
-    
+
     def getFileType(self, fileName):
         if ".txt" in fileName:
             return "text/plain"
@@ -33,52 +32,44 @@ class getFileList:
         if ".csv" in fileName:
             return "application/octet-stream"
         if ".log" in fileName:
-            return "application/octet-stream"            
+            return "application/octet-stream"
         return ''
 
-    def getFileListInFolder(self, currentDir, folder):
+    def getFileListInFolder(self, saveDir, folder):
         retDict = dict()
-        path = currentDir + "/" + folder
+        path = saveDir + folder
         files = os.listdir(path)
         files = [f for f in files if os.path.isfile(os.path.join(path, f))]
         idx = 0
         for f in files:
-            retDict[folder + "_" + str(idx)] = [folder + "/" + f, self.getFileType(f)]
+            retDict[folder + "_" +
+                    str(idx)] = [folder + "/" + f, self.getFileType(f)]
             idx += 1
         return retDict
 
-
     def getData(self):
+        com = comFunction.common()
         # 設定ファイルから必要な情報を取得する
-        # タグ
-        tag_ListFolder = '[b]'
-        tag_DataFolder = '[B]'
-        
-        # カレントディレクトリ取得
-        currentDir = '/'
-        try:
-            # Node-RED から呼び出し
-            currentDir = os.path.dirname(__file__) + '/'
-        except:
-            # jupyterNotebook から呼び出し
-            currentDir = os.path.dirname(os.path.abspath("__file__")) + '/'
+        # 先頭は必ず SaveDir にする
+        tagSaveDir = '[0]'
+        tagListFolder = '[b]'
+        tagDataFolder = '[B]'
 
-        listFolder = ''
-        dataFolder = ''
+        settingDict = com.getSettingData([
+            tagSaveDir,
+            tagListFolder,
+            tagDataFolder
+        ])
+        com.infoMsg(sys._getframe().f_code.co_name, json.dumps(settingDict))
+
+        if len(settingDict) <= 0:
+            com.errMsg(sys._getframe().f_code.co_name,
+                       'SettingData is none...')
+            return dict(), 1
+        saveDir = settingDict[tagSaveDir]
+        listFolder = settingDict[tagListFolder]
+        dataFolder = settingDict[tagDataFolder]
         retDict = dict()
-
-        # Setting.txtからフォルダ名を取得
-        try:
-            with open(currentDir + '_Setting.txt', mode='r') as f:
-                lines = f.readlines()
-                for l in lines:
-                    if l.startswith(tag_ListFolder, 0, 3):
-                        listFolder = l.replace(tag_ListFolder, '').rstrip()
-                    if l.startswith(tag_DataFolder, 0, 3):
-                        dataFolder = l.replace(tag_DataFolder, '').rstrip()
-        except:
-            print('[!!!ERROR!!!] Read Setting Text')
-            return retDict, 1
 
         if len(listFolder) <= 0:
             print('missing listFolderPath...')
@@ -88,19 +79,18 @@ class getFileList:
             return retDict, 1
 
         # フォルダ内にあるファイル名を取得
-        retDict.update(self.getFileListInFolder(currentDir, listFolder))
-        retDict.update(self.getFileListInFolder(currentDir, dataFolder))
+        retDict.update(self.getFileListInFolder(saveDir, listFolder))
+        retDict.update(self.getFileListInFolder(saveDir, dataFolder))
 
         return retDict, 0
 
 # %% [markdown]
 # ## 処理開始
 
+
 # %%
 if __name__ == '__main__':
     read = getFileList()
     ret, isError = read.getData()
     print(json.dumps(ret))
-    sys.exit(isError)   
-
-
+    sys.exit(isError)
